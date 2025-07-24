365f07b61b1ac9b41491670144c3bca7ec6706fb
ahmed-592 <ahmmeeddd592@gmail.com>
2025-07-24 11:56:04 +0300

    Edit NotFond Padge

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './Components/Home/Home'
 import About from './Components/About/About'
 import Contact from './Components/Contact/Contact'
 import Portfolio from './Components/Portfolio/Portfolio'
+import NotFounded from './Components/NotFounded/NotFounded'
 
 
  let routering = createBrowserRouter([
@@ -14,6 +15,7 @@ import Portfolio from './Components/Portfolio/Portfolio'
     {path: "/about" , element: <About/>},
     {path: "/portfolio" , element: <Portfolio/>},
     {path: "/contact" , element: <Contact/>},
+    {path: "*" , element: <NotFounded/>},
   ]}
 ])
 
---
+++ src/Components/NotFounded/NotFounded.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NotFounded() {
+  return <>
+  
+  <div className="py-5 d-flex flex-column justify-content-center align-items-center">
+    <h2>Not Found</h2>
+  <p>This page doesn’t exist.</p>
+  <p>If this is a mistake, let us know, and we will try to fix it!</p>
+  </div>
+  
+  </>
+}
